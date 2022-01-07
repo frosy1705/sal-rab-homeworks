fd4eabbb49bb3c9a81df9b359fd19c28f1abd385
Anastasiya Kulichkova <frosy1705@gmail.com>
2022-01-07 18:42:56 +0300

    task_1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -15,6 +15,9 @@ function addInCartMessage(title, price) {
     // выведите переменную message в консоль
     // test
 
+    let message = productName + " " + "за" + " " + productPrice + " " + "теперь в корзине!";
+    console.log(message);
+
     // Конец решения задания №1.1.
 
     return message;
@@ -28,6 +31,9 @@ function addInCartCountChange(value) {
     // создайте переменную newValue
     // присвойте ей значение oldValue
     // увеличьте значение newValue на 1
+
+    let newValue = oldValue
+    newValue++
 
     // Конец решения задания №1.2.
 
@@ -43,6 +49,10 @@ function addInCartSumChange(sum, delta) {
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
+
+    let newSum = oldSum + difference
+    let newSumText = newSum + " ₽"
+    newSumText = `${newSum} ₽`
 
     // Конец решения задания №1.3.
 
